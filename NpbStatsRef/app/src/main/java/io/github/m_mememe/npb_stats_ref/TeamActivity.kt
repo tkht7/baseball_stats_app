63c7d6217aafaae3c064fe0a7f1ea7f9a38fc4bf
tkht <takahito711@gmail.com>
2020-02-12 16:38:53 +0900

    androidのメインと選手一覧ページ(仮)

---
+++ NpbStatsRef/app/src/main/java/io/github/m_mememe/npb_stats_ref/TeamActivity.kt
@@ -0,0 +1,49 @@
+package io.github.m_mememe.npb_stats_ref
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.eclipsesource.json.Json
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import org.json.JSONArray
+
+class TeamActivity : AppCompatActivity() {
+    val URL = "http://weather.livedoor.com/forecast/webservice/json/v1?city=400040" //サンプル:ライブドアのお天気Webサービス
+//    val URL = "http://127.0.0.1:8000/api/batting/" //ローカルホストを叩いてみる
+    var result = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_team)
+
+        val intent: Intent = getIntent()
+        val teamName: String = intent.getStringExtra("buttonText")
+        val textView: TextView = findViewById(R.id.textView)
+        textView.setText(teamName + "の選手一覧表示")
+
+        // Activity起動時に実行
+//        onParallelGetInfo()
+    }
+
+    //非同期処理でHTTP GETを実行
+//    fun onParallelGetInfo() = GlobalScope.launch(Dispatchers.Main) {
+//        val http = HttpUtil()
+//        //Mainスレッドでネットワーク関連処理を実行するとエラーになるためBackgroundで実行
+//        async(Dispatchers.Default) { http.httpGET1(URL) }.await().let {
+//            //minimal-jsonを使って　jsonをパース
+//            val result = Json.parse(it).asObject()
+//            val textView = findViewById(R.id.textView) as TextView
+////            textView.setText(result.get("description").asObject().get("text").asString())
+//            val result2 = result.get("results")
+//            val result3: JSONArray = JSONArray(result2)
+//            val result4 = result3.get("name")
+//            textView.setText(result.get("count").toString())
+//        }
+//    }
+
+
+}
