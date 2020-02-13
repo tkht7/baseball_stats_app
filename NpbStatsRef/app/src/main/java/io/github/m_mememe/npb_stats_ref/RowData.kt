3425dee1e8b6a22afc17ec8d347034452ce5df9d
tkht <takahito711@gmail.com>
2020-02-14 06:38:58 +0900

    選手一覧ページ作成

---
+++ NpbStatsRef/app/src/main/java/io/github/m_mememe/npb_stats_ref/RowData.kt
@@ -0,0 +1,10 @@
+package io.github.m_mememe.npb_stats_ref
+
+class RowData {
+    var name: String = ""
+    var data1: String = ""  //後で入れるデータの名前に変える
+    var data2: String = ""
+    var data3: String = ""
+    var data4: String = ""
+    var data5: String = ""
+}
--- NpbStatsRef/app/src/main/java/io/github/m_mememe/npb_stats_ref/TeamActivity.kt
+++ NpbStatsRef/app/src/main/java/io/github/m_mememe/npb_stats_ref/TeamActivity.kt
@@ -3,50 +3,80 @@ package io.github.m_mememe.npb_stats_ref
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.TextView
+import android.view.View
+import android.widget.*
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.eclipsesource.json.Json
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
-import org.json.JSONArray
-import org.json.JSONObject
-import java.io.BufferedReader
+import com.google.android.material.snackbar.Snackbar
 
 class TeamActivity : AppCompatActivity() {
 //    val URL = "http://weather.livedoor.com/forecast/webservice/json/v1?city=400040" //サンプル:ライブドアのお天気Webサービス
-    val URL = "http://10.0.2.2:8000/api/batting/" //ローカルホストを叩いてみる
+    val URL = "http://10.0.2.2:8000/api/batting/?offset=240" //ローカルホストを叩いてみる
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_team)
 
-//        val intent: Intent = getIntent()
-//        val teamName: String = intent.getStringExtra("buttonText")
-//        val textView: TextView = findViewById(R.id.textView)
-//        textView.setText(teamName + "の選手一覧表示")
+        val intent: Intent = getIntent()
+        val teamName: String = intent.getStringExtra("buttonText")
+        val textView: TextView = findViewById(R.id.teamName)
+        textView.setText(teamName)
 
         // Activity起動時に実行
         onParallelGetInfo()
     }
 
+    fun onClickData(tappedView: View, rowData: RowData) {
+        Snackbar.make(tappedView, "${rowData.name}の個人ページに飛ぶ", Snackbar.LENGTH_LONG).setAction("Action", null).show()
+    }
+
     //非同期処理でHTTP GETを実行
     fun onParallelGetInfo() = GlobalScope.launch(Dispatchers.Main) {
         val http = HttpUtil()
+
         //Mainスレッドでネットワーク関連処理を実行するとエラーになるためBackgroundで実行
         async(Dispatchers.Default) { http.httpGET1(URL) }.await().let {
             //minimal-jsonを使って　jsonをパース
             val result = Json.parse(it).asObject()
-            val textView = findViewById(R.id.textView) as TextView
-
             val result2 = result.get("results").asArray()
-            val result3 = result2[0].asObject()
-            val result4 = result3.get("name").asString()
-            textView.setText(result4)
 
+            fun createDataList(): List<RowData> {
+                val dataList = mutableListOf<RowData>()
+                for (res in result2) {
+                    val data: RowData = RowData().also {stats ->
+                        stats.name = res.asObject().get("name").asString()
+                        stats.data1 = res.asObject().get("games").toString()
+                        stats.data2 = res.asObject().get("plate_appearances").toString()
+                        stats.data3 = res.asObject().get("runs").toString()
+                        stats.data4 = res.asObject().get("hits").toString()
+                        stats.data5 = res.asObject().get("homeruns").toString()
+                    }
+                    dataList.add(data)
+                }
+                return dataList
+            }
+
+            viewAdapter = RecyclerAdapter(createDataList(), object : RecyclerAdapter.ListListener{
+                override fun onClickData(tappedView: View, rowData: RowData) {
+                    this@TeamActivity.onClickData(tappedView, rowData)
+                }
+            })
+            viewManager = LinearLayoutManager(this@TeamActivity, LinearLayoutManager.VERTICAL, false)
+
+            recyclerView = findViewById<RecyclerView>(R.id.recyclerView).apply {
+                setHasFixedSize(true)
+                layoutManager = viewManager
+                adapter = viewAdapter
+            }
         }
     }
-
-
 }
